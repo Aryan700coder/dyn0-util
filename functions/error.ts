8ea7c1ceca71cb87a24e63da8267ad924d96df09
Aryan <123funmedia@gmail.com>
2022-03-12 11:06:13 +0530

    Error add

---
+++ functions/error.ts
@@ -0,0 +1,20 @@
+import Client from "../"
+import { Collection, MessageEmbed } from "discord.js";
+module.exports = async (client: typeof Client) => {
+    process.on('unhandledRejection', (reason, p) => {
+        console.log(' [antiCrash] :: Unhandled Rejection/Catch');
+        console.log(reason, p);
+    });
+    process.on("uncaughtException", (err, origin) => {
+        console.log(' [antiCrash] :: Uncaught Exception/Catch');
+        console.log(err, origin);
+    }) 
+    process.on('uncaughtExceptionMonitor', (err, origin) => {
+        console.log(' [antiCrash] :: Uncaught Exception/Catch (MONITOR)');
+        console.log(err, origin);
+    });
+    process.on('multipleResolves', (type, promise, reason) => {
+        console.log(' [antiCrash] :: Multiple Resolves');
+        console.log(type, promise, reason);
+    });
+}
